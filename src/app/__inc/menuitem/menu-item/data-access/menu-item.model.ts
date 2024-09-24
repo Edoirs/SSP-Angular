5028d5858cb927014b8a2adda82ba1b3ef3802de
Joseph Chikeme <jchikeme@gmail.com>
2024-09-24 19:18:36 +0100

    updating menu

--- src/app/__inc/menuitem/menu-item/data-access/menu-item.model.ts
+++ src/app/__inc/menuitem/menu-item/data-access/menu-item.model.ts
@@ -1,6 +1,12 @@
 export interface MenuItemInterface {
   name: string
-  url: string | string[]
+  url?: string | string[]
   faIcon: string
-  children?: {name: string; url: string | string[]; faIcon: string}[]
+  faIconType?: "solid" | "outline"
+  children?: {
+    name: string
+    url?: string | string[]
+    faIcon: string
+    faIconType?: "solid" | "outline"
+  }[]
 }
--- src/app/__inc/menuitem/menu/menu.component.ts
+++ src/app/__inc/menuitem/menu/menu.component.ts
@@ -9,6 +9,90 @@ import {MenuItemInterface} from "../menu-item/data-access/menu-item.model"
 export class MenuComponent {
   readonly menuList: MenuItemInterface[] = [
     {
+      // 0
+      name: "Dashboard",
+      url: ["/admin", "dashboard"],
+      faIcon: "fa-tachometer-alt",
+    },
+    {
+      // 1
+      name: "Businesses",
+      url: ["/admin", "businesses"],
+      faIcon: "fa-building",
+    },
+    {
+      // 2
+      name: "Monthly Remittance",
+      url: ["/admin", "schedules"],
+      faIcon: "fa-file-invoice",
+      children: [
+        {
+          name: "Employees",
+          url: ["admin", "employee-schedule"],
+          faIcon: "fa-circle",
+          faIconType: "outline",
+        },
+        {
+          name: "Deleted Employees",
+          url: ["admin", "deleted-employees"],
+          faIcon: "fa-circle",
+          faIconType: "outline",
+        },
+        {
+          name: "Schedules",
+          url: ["admin", "schedules"],
+          faIcon: "fa-circle",
+          faIconType: "outline",
+        },
+        {
+          name: "Assessments",
+          url: ["admin", "assessments"],
+          faIcon: "fa-circle",
+          faIconType: "outline",
+        },
+      ],
+    },
+    {
+      // 3
+      name: "Form H1 (Annual Return)",
+      url: ["/admin", "schedules"],
+      faIcon: "fa-file-invoice",
+      children: [
+        {
+          name: "Employees",
+          url: ["admin", "employee-schedule"],
+          faIcon: "fa-circle",
+          faIconType: "outline",
+        },
+        {
+          name: "Deleted Employees",
+          url: ["admin", "deleted-employees"],
+          faIcon: "fa-circle",
+          faIconType: "outline",
+        },
+        {
+          name: "Schedules",
+          url: ["admin", "schedules"],
+          faIcon: "fa-circle",
+          faIconType: "outline",
+        },
+        {
+          name: "Assessments",
+          url: ["admin", "assessments"],
+          faIcon: "fa-circle",
+          faIconType: "outline",
+        },
+      ],
+    },
+    {
+      name: "testing",
+      url: ["/admin", "schedules"],
+      faIcon: "fa-circle",
+      children: [
+        {name: "dashboard", url: "/admin/dashboard", faIcon: "fa-circle"},
+      ],
+    },
+    {
       name: "testing",
       url: ["/admin", "schedules"],
       faIcon: "fa-circle",
