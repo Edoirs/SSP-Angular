e9c23d183d6e246ff12d4709e7f88d6d910f6214
Joseph Chikeme <jchikeme@gmail.com>
2024-11-15 15:41:32 +0100

    added system settings table

--- src/app/admin/features/settings/system-settings/system-settings.component.ts
+++ src/app/admin/features/settings/system-settings/system-settings.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal} from "@angular/core"
+import {MatPaginatorModule, PageEvent} from "@angular/material/paginator"
+import {TokenService} from "@shared/services/token.service"
+import {NgxSkeletonLoaderModule} from "ngx-skeleton-loader"
 
 @Component({
-  selector: 'app-system-settings',
+  selector: "app-system-settings",
   standalone: true,
-  imports: [],
-  templateUrl: './system-settings.component.html',
-  styleUrl: './system-settings.component.css'
+  imports: [MatPaginatorModule, NgxSkeletonLoaderModule],
+  providers: [],
+  templateUrl: "./system-settings.component.html",
+  styleUrl: "./system-settings.component.css",
 })
 export class SystemSettingsComponent {
+  public readonly tokenService = inject(TokenService)
 
+  pageSize = signal(10)
+  totalLength = signal(500)
+  pageIndex = signal(0)
+
+  handlePageEvent(event: PageEvent) {}
 }
